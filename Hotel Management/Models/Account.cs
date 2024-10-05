1e70ac52e27e44c65028bbb6945f880f9a507caf
Le Ngoc Cuong <lengoccuong02042004@gmail.com>
2024-10-05 12:03:56 +0700

    Thêm model Account

---
+++ Hotel Management/Models/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Hotel_Management.Models
+{
+    [Table("TAIKHOAN")]
+    public class Account
+    {
+        [Column("MANV")]
+        public int Employee { get; set; }
+        [Column("MATKHAU")]
+        public string Password { get; set; }
+        [Column("VAITRO")]
+        public string Role { get; set; }
+        [Column("NGAYTAO")]
+        public DateTime CreatedAt { get; set; }
+        [Column("NGAYDOIMK")]
+        public DateTime PasswordUpdatedAt { get; set; }
+
+        public Account() { }
+    }
+}
